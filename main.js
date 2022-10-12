3790892dd60ab682b01fd743d4b104a61672e802
JurRodi <rodijurrien@gmail.com>
2022-10-12 08:59:22 +0200

    Feat: add orbitControls

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -24,10 +25,13 @@ const ambientLight = new THREE.AmbientLight(0xffffff);
 
 scene.add(pointLight, ambientLight);
 
-camera.position.z = 5;
+const controls = new OrbitControls(camera, renderer.domElement);
+
+camera.position.z = 30;
 
 function animate() {
 	requestAnimationFrame( animate );
+  controls.update();
 	renderer.render( scene, camera );
 }
 animate();
